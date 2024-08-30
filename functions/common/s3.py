0414d9d4531117d9ab8de416d8f9fd6f796b7220
Nicole Hwang <nicole.hw.hwang@gmail.com>
2024-08-30 13:24:09 -0500

    added list_files-in-prefix function

--- functions/common/s3.py
+++ functions/common/s3.py
@@ -20,14 +20,22 @@ class S3BucketConnector():
         """
         self._logger = logging.getLogger(__name__)
         self.endpoint_url = endpoint_url
-        self.session = boto3.Session(aws_access_key = os.environ[access_key],
+        self.session = boto3.Session(aws_access_key_id = os.environ[access_key],
                                      aws_secret_access_key = os.environ[secret_key])
         # protected var.
         self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
     
-    def list_files_in_prefix(self):
-        pass
+    def list_files_in_prefix(self, prefix: str):
+        """
+        Listing all files with a prefix on S3 bucket
+        :param prefix: prefix to filter by on the S3 bucket
+
+        returns:
+            files: list of all the file names containing the prefix in the key
+        """
+        files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
+        return files
 
     def read_csv_to_df(self):
         pass
